3de71c432f09451f3c56831cafd249442056ba89
J.M.RUIZ.TELLEZ <53316415+alucard309509@users.noreply.github.com>
2022-07-04 17:49:50 -0500

    Get

--- app/src/main/java/com/alucard/jmrt_sps/Interface/NewsService.kt
+++ app/src/main/java/com/alucard/jmrt_sps/Interface/NewsService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 
 interface NewsService {
 
-
+    //Get
     @get:GET("v2/sources?apiKey=acae9d34f05f4f4d9c6d8487234b1aa1")
     //website es nuestro modelo de clase
     val sources: Call<Website>
