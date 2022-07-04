a4b0b7f410fa108ff76eee9481376c44f55e72d2
J.M.RUIZ.TELLEZ <53316415+alucard309509@users.noreply.github.com>
2022-07-04 17:45:35 -0500

    Shared Preferences

--- app/src/main/java/com/alucard/jmrt_sps/data/Prefs.kt
+++ app/src/main/java/com/alucard/jmrt_sps/data/Prefs.kt
@@ -3,7 +3,7 @@ package com.alucard.jmrt_sps.data
 import android.content.Context
 
 class Prefs(val context: Context) {
-
+        //Clase que se encarga de los process de guardado en shared preferences con la ubicacion latitud y longitud del usuario
     val SHAREDLOCATATION = "MYLOCALITATION"
     val SHARED_USER_LOCALITATION = "USERPOSITION"
     val storage = context.getSharedPreferences(SHAREDLOCATATION, 0)
